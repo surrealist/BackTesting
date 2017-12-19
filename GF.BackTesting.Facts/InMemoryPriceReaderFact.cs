e06d262177d7a4026e0d076f4a3dd9343e45fdfd
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2017-12-19 16:20:13 +0700

    Initial commit

---
+++ GF.BackTesting.Facts/InMemoryPriceReaderFact.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace GF.BackTesting.Facts {
+  public class InMemoryPriceReaderFact {
+
+    [Fact]
+    public void NoSeedPrice() {
+      var reader = new InMemoryPriceReader();
+      decimal price = 0m;
+      int count = 0;
+       
+      reader.NewPrice += (sender, e) => {
+        price = e.Last;
+        count++;
+      };
+
+      reader.Start();
+
+      Assert.Equal(0, count);
+      Assert.Equal(0m, price);
+    }
+
+
+    [Fact]
+    public void SinglePrice() {
+      var reader = new InMemoryPriceReader();
+      decimal price = 0m;
+
+      reader.AddSeedPrice(last: 15.0m);
+      reader.NewPrice += (sender, e) => {
+        price = e.Last;
+      };
+
+      reader.Start();
+
+      Assert.Equal(15.0m, price);
+    }
+
+    [Fact]
+    public void ThreePrices() {
+      var reader = new InMemoryPriceReader();
+      decimal price = 0m;
+      int count = 0;
+
+      reader.AddSeedPrice(last: 15.0m);
+      reader.AddSeedPrice(last: 16.0m);
+      reader.AddSeedPrice(last: 14.0m);
+
+      reader.NewPrice += (sender, e) => {
+        price = e.Last;
+        count++;
+      };
+
+      reader.Start();
+
+      Assert.Equal(3, count);
+      Assert.Equal(14.0m, price);
+    }
+
+  }
+}
---
+++ GF.BackTesting/CandleStickItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GF.BackTesting {
+  public class CandleStickItem {
+    public DateTime Date { get; set; }
+    public decimal Open { get; set; }
+    public decimal High { get; set; }
+    public decimal Close { get; set; }
+    public decimal Low { get; set; }
+    public CandleStickColor Color { get; set; }
+  }
+}
